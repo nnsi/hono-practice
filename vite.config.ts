c2284c37131502726b718a141049d47042fbe1f8
nsi <n.or.fates@gmail.com>
2024-12-30 00:03:06 +0900

    たのしい

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,29 @@
-import path from "path";
-
 import build from "@hono/vite-build/node";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
-
-const resolve = {
-  alias: {
-    "@": path.resolve("./"),
-    "@hooks": path.resolve("./frontend/src/hooks"),
-    "@components": path.resolve("./frontend/src/components"),
-  },
-};
 
 export default defineConfig(({ mode }) => {
   if (mode === "server") {
     return {
-      test: {
-        setupFiles: ["./backend/test.setup.ts"],
-      },
-      resolve,
       plugins: [
+        tsconfigPaths(),
         build({
           entry: "./backend/index.ts",
           outputDir: "./dist-backend",
         }),
       ],
+      test: {
+        setupFiles: ["./backend/test.setup.ts"],
+      },
     };
+    // biome-ignore lint/style/noUselessElse: <explanation>
   } else {
     return {
-      resolve,
-      plugins: [TanStackRouterVite(), react()],
+      plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
       build: {
         outDir: "dist-frontend",
         emptyOutDir: true,
