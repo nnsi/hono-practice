df17a869b934de7c00409745aa5563ed178d4276
nsi <n.or.fates@gmail.com>
2025-01-12 09:48:38 +0900

    いけるかな？

--- backend/server.cf.ts
+++ backend/server.cf.ts
@@ -7,11 +7,11 @@ import { app } from "./app";
 
 import type { SafeEnvs } from "./config";
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql });
-
 export default {
   fetch: (req: Request, env: SafeEnvs, ctx: ExecutionContext) => {
+    const sql = neon(env.DATABASE_URL);
+    const db = drizzle({ client: sql });
+
     const envWithDB = { ...env, DB: db };
     return app.fetch(req, envWithDB, ctx);
   },
