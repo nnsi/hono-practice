bf8c01d70d6ab3805488f8cc18b29066aeac5109
nsi <n.or.fates@gmail.com>
2025-01-15 00:38:39 +0900

    サイズ軽くする

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import build from "@hono/vite-build/cloudflare-workers";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
@@ -31,12 +32,19 @@ export default defineConfig(({ mode }) => {
   }
 
   // frontend
+  process.env.NODE_ENV = "production";
   return {
     publicDir: "frontend/public",
     plugins: [tsconfigPaths(), TanStackRouterVite(), react()],
+    define: {
+      "process.env.NODE_ENV": JSON.stringify("production"),
+    },
     build: {
       outDir: "dist-frontend",
       emptyOutDir: true,
+      rollupOptions: {
+        plugins: [visualizer()],
+      },
     },
   };
 });
