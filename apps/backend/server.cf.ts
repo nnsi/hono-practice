cef9257b0044c88931994547fb12cabc01cc6c02
nsi <n.or.fates@gmail.com>
2025-02-02 03:05:06 +0900

    ねむい

--- apps/backend/server.cf.ts
+++ apps/backend/server.cf.ts
@@ -1,8 +1,8 @@
 import type { ExecutionContext } from "hono";
 
 import * as schema from "@infra/drizzle/schema";
-import { neon } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-http";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 import { app } from "./app";
 
@@ -10,8 +10,8 @@ import type { Config } from "./config";
 
 export default {
   async fetch(req: Request, env: Config, ctx: ExecutionContext) {
-    const sql = neon(env.DATABASE_URL);
-    const db = drizzle({ client: sql, schema: schema });
+    const pool = new Pool({ connectionString: env.DATABASE_URL });
+    const db = drizzle({ client: pool, schema: schema });
 
     return app.fetch(req, { ...env, DB: db }, ctx);
   },
