ca9cf5c5c1c3f824cadd47ecf8d1c12f75ede555
nsi <n.or.fates@gmail.com>
2025-02-02 02:36:04 +0900

    二日酔いで1日つぶれた

--- apps/backend/infra/drizzle/drizzleTransaction.ts
+++ apps/backend/infra/drizzle/drizzleTransaction.ts
@@ -4,14 +4,28 @@ import type { QueryExecutor } from "./drizzleInstance";
 export function newDrizzleTransactionRunner(
   db: QueryExecutor,
 ): TransactionRunner {
-  return {
-    async run(repositories, operation) {
-      return db.transaction(async (txDb) => {
-        const txRepoArray = repositories.map((repo) => repo.withTx(txDb));
-        const mergedTxRepos = Object.assign({}, ...txRepoArray);
+  try {
+    return {
+      async run(repositories, operation) {
+        return db.transaction(async (txDb) => {
+          const txRepoArray = repositories.map((repo) => repo.withTx(txDb));
+          const mergedTxRepos = Object.assign({}, ...txRepoArray);
 
-        return await operation(mergedTxRepos);
-      });
-    },
-  };
+          return await operation(mergedTxRepos);
+        });
+      },
+    };
+  } catch (e) {
+    // NOTE:db.transactionがサポートされてない場合は非トランザクションで実行
+    return {
+      async run(repositories, operation) {
+        return (async () => {
+          const dbRepoArray = repositories.map((repo) => repo.withTx(db));
+          const mergedTxRepos = Object.assign({}, ...dbRepoArray);
+
+          return await operation(mergedTxRepos);
+        })();
+      },
+    };
+  }
 }
